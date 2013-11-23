c5c9be2b4964f6f8fce84f645c7903eddfa8042a
Steve Roggenkamp <roggenkamps@acm.org>
2013-11-23 15:11:25 -0500

    Users seem to be working.

--- DirectMarket/config/routes.rb
+++ DirectMarket/config/routes.rb
@@ -1,7 +1,13 @@
 DirectMarket::Application.routes.draw do
   
+  resources :users
+
   root to: 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
+
+#   get '/users(.:format)' => 'users#create'
+
+#  user GET  /users/:id(.:format)  users#show
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
